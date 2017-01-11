184cf75ca5940e42b97c98e42673830717b4c25c
meeksfred <maxfred0025@gmail.com>
2017-01-10 16:32:27 -0800

    class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react'; // { Component } is the same as const Component = React.Component ES6<
 
-const SearchBar = () => {
-  return <input />;
-};
+class SearchBar extends Component {
+  render() {
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+}
 
 export default SearchBar;
