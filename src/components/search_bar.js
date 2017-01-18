c819e2000b11a672c627063eb86fcad72c5498e6
meeksfred <maxfred0025@gmail.com>
2017-01-18 11:02:17 -0800

    using controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'; // { Component } is the same as const Component = React.Component ES6<
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: 'Start' }; // only place this syntax will appear, my guess is
+    // because this is where we initialized state for the component.
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    // to change the state anywhere else, we use this.setState.
+    return (
+      <div>
+        <input
+          value={this.state.term} // before, the value attribute on input wasn't actually changing or being set by the onChange event.
+          onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
