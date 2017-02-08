139851ab67989528d02c98922a53b6c4086a088d
meeksfred <maxfred0025@gmail.com>
2017-02-08 14:43:56 -0800

    some styling for video list and search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,14 +4,14 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { term: 'Start' }; // only place this syntax will appear, my guess is
+    this.state = { term: '' }; // only place this syntax will appear, my guess is
     // because this is where we initialized state for the component.
   }
 
   render() {
     // to change the state anywhere else, we use this.setState.
     return (
-      <div>
+      <div className="search-bar">
         <input
           value={this.state.term} // before, the value attribute on input wasn't actually changing or being set by the onChange event.
           onChange={event => this.setState({ term: event.target.value })} />
